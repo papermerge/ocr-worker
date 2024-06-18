aebd04b3ee6b6fe6d1593d2315dd40a72dc37821
Eugen Ciur <eugen@papermerge.com>
2024-06-18 07:40:47 +0200

    add dockerfile/github workflows

---
+++ docker/run.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CMD="$1"
+
+if [ -z $CMD ]; then
+  echo "No command specified"
+  exit 1
+fi
+
+exec_worker() {
+  export VIRTUAL_ENV=/app/venv
+  cd /app && poetry run celery -A ocrworker.celery_app worker ${OCR_WORKER_ARGS}
+}
+
+case $CMD in
+  worker)
+    exec_worker
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
